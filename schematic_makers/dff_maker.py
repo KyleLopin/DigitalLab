480ae176312486db9a03305a971fcada42ebe173
kylelopin <kylelopin@gmail.com>
2025-10-22 04:59:20 -0500

    Have shift register started, save point with working clk and select pins

--- schematic_makers/dff_maker.py
+++ schematic_makers/dff_maker.py
@@ -15,10 +15,10 @@ class Mux4(ic.Multiplexer):
     def __init__(self, **kwargs):
         super().__init__(
             pins=[
-                ic.IcPin(name='D0', side='L'),
-                ic.IcPin(name='D1', side='L'),
-                ic.IcPin(name='D2', side='L'),
                 ic.IcPin(name='D3', side='L'),
+                ic.IcPin(name='D2', side='L'),
+                ic.IcPin(name='D1', side='L'),
+                ic.IcPin(name='D0', side='L'),
                 ic.IcPin(name='Q',  side='R'),
                 ic.IcPin(name='S0', side='B'),
                 ic.IcPin(name='S1', side='B'),
@@ -34,23 +34,42 @@ class Mux4(ic.Multiplexer):
 with sd.Drawing() as d:
 
     ffs = []
+    muxs = []
     n = 4
     seg=None
     for i in range(n):
         if seg:
-            ff = d.add(ic.DFlipFlop(size=(2,3)).anchor('D').at(seg.end))
+            ff = d.add(ic.DFlipFlop(size=(3,4), preclr=True,
+                                    preclrinvert=False).anchor('D').at(seg.end))
         else:
-            ff = d.add(ic.DFlipFlop(size=(2,3)))
+            ff = d.add(ic.DFlipFlop(size=(3,4), preclr=True,
+                                    preclrinvert=False))
         ffs.append(ff)
         d.add(lines.Line().at(ffs[i].Q).right(d.unit/4))
         d.add(lines.Line().up(d.unit))
         seg = d.add(lines.Line().right(d.unit / 4))
-        mux = d.add(Mux4(inputs=4).anchor('D0').at(seg.end).scale(0.7))
-
+        mux = d.add(Mux4(inputs=4).anchor('D3').at(seg.end).scale(0.7))
+        muxs.append(mux)
         d.add(lines.Line().right(d.unit / 4).at(mux.Q))
         d.add(lines.Line().down().toy(ff.Q))
         seg = d.add(lines.Line().right(d.unit / 4))
 
+    # add clock line
+    for ff in ffs:
+        d.add(lines.Line().down(d.unit / 2).at(ff.CLK))
+    d.add(lines.Line().left().tox(ffs[0].CLK))
+    d.add(lines.Line().left().label("CLK", loc="left"))
+
+    # add select lines
+    for mux in muxs:
+        d.add(lines.Line().down(d.unit/4).at(mux.S0).dot())
+        # d.add(elm.Node())
+    d.add(lines.Line().left().tox(muxs[0].S0))
+    d.add(lines.Line().left(d.unit * 3).label("S0", loc="left"))
+    for mux in muxs:
+        d.add(lines.Line().down(d.unit / 6).at(mux.S1).dot())
+    d.add(lines.Line().left().tox(muxs[0].S1))
+    d.add(lines.Line().left(d.unit * 3.2).label("S1", loc="left"))
 
     d.show()
 
