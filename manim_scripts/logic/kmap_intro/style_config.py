8fcb02c09fbc52d07301f07aaceac910787729e0
kylelopin <kylelopin@gmail.com>
2026-01-09 09:55:16 -0600

    Script to keep styles consistant between files

---
+++ manim_scripts/logic/kmap_intro/style_config.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2025 Kyle Lopin (Naresuan University) <kylelopin@gmail.com>
+
+"""
+
+"""
+
+__author__ = "Kyle Vitautas Lopin"
+
+
+numbers_font = "Menlo"
+num_font_size = 24
+text_font_size = 36
+ex_lbl_font_size = 24
+imp_stroke_width = 8
+# text_font = "Felix Titling"
+text_font = "Monaco"
+fancy_font = "Noteworthy"
+header1_size = 38
+header2_size = 32
+SET_CUE = True
